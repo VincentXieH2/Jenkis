a852912630f28c88a774bdae97cdf49c587f070c
vincent.xie.h2 <vincent.xie.h2@gmail.com>
2017-11-25 14:21:11 +0800

    New Test

--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-        result := Add(1, 3)
+        result := Add(1, 5)
         if result != 4 {
                 t.Errorf("should be 4 but %d", result)
         }
